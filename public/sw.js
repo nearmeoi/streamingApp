285b368bffc0e43c2af98e010ae49c73f1df02a3
HuaStream Developer <developer@huastream.app>
2026-01-21 09:25:20 +0700

    feat: setup pwa manifest and sw, and rename output apk

---
+++ public/sw.js
@@ -0,0 +1,21 @@
+// Basic Service Worker for PWA Installability
+const CACHE_NAME = 'huastream-v1';
+const urlsToCache = [
+    '/',
+    '/icon.png',
+    '/favicon.svg'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
